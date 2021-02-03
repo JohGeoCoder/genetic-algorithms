0ae10fe33d34987ed000e0328a9f63b2f1ba9f2c
John George <johndouglasgeorge@gmail.com>
2021-02-02 23:48:05 -0500

    Generate the warehouse population

--- TaskRunner/Program.cs
+++ TaskRunner/Program.cs
@@ -4,6 +4,7 @@ using System.Diagnostics.Tracing;
 using System.Linq;
 using System.Threading.Tasks;
 using TaskRunner.GeneticStructures;
+using TaskRunner.Organisms;
 using TaskRunner.Populations;
 
 namespace TaskRunner
@@ -14,13 +15,59 @@ namespace TaskRunner
         {
             var rng = new Random();
 
-            var initialPopulation = new IncrementingBoard[200];
-            for(int i = 0; i < initialPopulation.Length; i++)
+            var products = Enumerable.Range(0, 10000).Select(x => new Product
             {
-                initialPopulation[i] = new IncrementingBoard(200);
-            }
+                Id = x,
+                ProductName = new string(Enumerable.Range(0, 9).Select(n => (char)('a' + rng.Next(0, 26))).ToArray())
+            }).ToArray();
 
-            var runner = new Runner(initialPopulation, 5000);
+            var pickTickets = Enumerable.Range(0, 100).Select(x =>
+            {
+                var pickTicketSize = rng.Next(1, 20);
+                var productIds = new int[pickTicketSize];
+
+                var pickIndex = 0;
+                for(int i = 0; i < products.Length; i++)
+                {
+                    //Probability this product will be next on the pick ticket.
+                    var selectThreshold = (double)pickTicketSize / (products.Length - i);
+
+                    if(rng.NextDouble() < selectThreshold)
+                    {
+                        var productId = products[i].Id;
+
+                        productIds[pickIndex] = productId;
+
+                        pickIndex++;
+                    }
+
+                    //If we've picked all the products for the pick ticket, exit the loop
+                    if(pickIndex >= pickTicketSize)
+                    {
+                        break;
+                    }
+                }
+
+                var pickTicket = new PickTicket
+                {
+                    ProductIds = productIds
+                };
+
+                return pickTicket;
+            })
+                .ToArray();
+
+            var initialPopulation = Enumerable.Range(0, 199)
+                .Select(x => new Warehouse(aisleCount: 200, aisleDepth: 150, products: products, pickTickets: pickTickets))
+                .ToArray();
+
+            //var initialPopulation = new IncrementingBoard[200];
+            //for(int i = 0; i < initialPopulation.Length; i++)
+            //{
+            //    initialPopulation[i] = new IncrementingBoard(200);
+            //}
+
+            var runner = new Runner(initialPopulation, 100);
             runner.Start();
         }
     }
